1d2232c6b9b5c9e725a0e606f2ee8925eb9c803b
Kanguros <edgar.ostrowski@gmail.com>
2016-12-29 20:46:26 +0100

    Created method for drawing clock

--- timer.py
+++ timer.py
@@ -1,32 +1,44 @@
 import pygame
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 
 class EscapeRoomTimer(object):
-    def __init__(self):
-        pass
-
-
-    def run(self):
-        BLACK = (0, 0, 0)
-        WHITE = (255, 255, 255)
-        RED = (255, 0, 0)
-
-        frame_rate = 60
-        total_time = 90  # sec
-
+    def __init__(self, total_time):
         pygame.init()
-
-        screen_size = [700, 500]
-        screen = pygame.display.set_mode(screen_size)
-
         pygame.display.set_caption("Escape Room")
 
-        clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode([700, 500])
+        self.clock = pygame.time.Clock()
+        self._frame_rate = 60
+        self.total_time = total_time
+
+    def _draw_clock(self):
         font = pygame.font.SysFont('Calibri', 50, True, False)
+        # Calculate text area size for '00:00' so that the
+        # text does not float around when time value is changing.
+        # Maybe it would be better to use a fixed-width font.
         text = font.render('00:00', True, WHITE)
         text_offset = [text.get_width() / 2, text.get_height() / 2]
 
+        minutes = self.total_time // 60
+        seconds = self.total_time % 60
+
+        output_str = '{0:02}:{1:02}'.format(minutes, seconds)
+
+        if minutes >= 1:
+            text_color = WHITE
+        else:
+            text_color = RED
+
+        text = font.render(output_str, True, text_color)
+
+        self.screen.blit(text, [self.screen.get_width() / 2 - text_offset[0],
+                                self.screen.get_height() / 2 - text_offset[1]])
+
+    def run(self):
         # Call user event every 1 sec
         pygame.time.set_timer(pygame.USEREVENT + 1, 1000)
 
@@ -38,32 +50,17 @@ class EscapeRoomTimer(object):
                 if event.type == pygame.QUIT:
                     done = True
                 if event.type == pygame.USEREVENT + 1 and not pause:
-                    if total_time:
-                        total_time -= 1
+                    if self.total_time:
+                        self.total_time -= 1
                     else:
-                        total_time = 0
+                        self.total_time = 0
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         pause = False
 
-            screen.fill(BLACK)
-
-            minutes = total_time // 60
-            seconds = total_time % 60
-
-            output_str = '{0:02}:{1:02}'.format(minutes, seconds)
-
-            if minutes >= 1:
-                text_color = WHITE
-            else:
-                text_color = RED
-
-            text = font.render(output_str, True, text_color)
-
-            screen.blit(text, [screen_size[0] / 2 - text_offset[0],
-                               screen_size[1] / 2 - text_offset[1]])
-
-            clock.tick(frame_rate)
+            self.screen.fill(BLACK)
+            self._draw_clock()
+            self.clock.tick(self._frame_rate)
 
             pygame.display.flip()
 
@@ -71,5 +68,5 @@ class EscapeRoomTimer(object):
 
 
 if __name__ == '__main__':
-    timer = EscapeRoomTimer()
+    timer = EscapeRoomTimer(total_time=90)
     timer.run()
